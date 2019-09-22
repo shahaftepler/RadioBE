860525dc583e150b598fef9a3471fe54f07b650c
Yarden Swisa <YardenSwisa88@gmail.com>
2019-09-22 20:43:25 +0300

    make order generalScreens

--- app/src/main/java/com/example/radiobe/generalScreens/Settings.java
+++ app/src/main/java/com/example/radiobe/generalScreens/Settings.java
@@ -3,21 +3,7 @@ package com.example.radiobe.generalScreens;
 import android.os.Bundle;
 
 import com.example.radiobe.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
-import android.view.View;
-
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class Settings extends AppCompatActivity {
 
